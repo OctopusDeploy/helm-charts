12f477325efee1ee2d67aec858d96cc217554d0e
Trent Mohay <trent.mohay@octopus.com>
2024-08-08 13:59:21 +1000

    a script which should go from 1 to 2

---
+++ charts/kubernetes-agent/migrations/upgrade_v1_to_v2.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This script is responsible for migrating a v1 Kubernetes Agent to v2.
+# The primary concern with this migration, is that some Values have changed location
+# in the Values file.
+# This script:
+## extracts values from currently installed kuberentes agent (in json)
+## transforms the values into the shape expected by a V2 install, and stores in a helm override file using a jq filter
+## upgrades the helm chart - by resetting the release's values, and applying the migrated values created in prior step
+OLDIFS=$IFS
+IFS='#'
+FILTER="{
+  name: .agent.targetName,
+  deploymentTarget: {
+     enabled: true,
+     initial: {
+       defaultNamespace: .agent.defaultNamespace,
+       tags: .agent.targetRoles,
+       tenantTags: .agent.targetTentantTags,
+       tenantedDeploymentParticipation: .agent.targetTenantedDeploymentParticipation,
+       tenants: .agent.targetTenants
+     }
+  },
+  scriptPods: {
+    deploymentTarget: {
+      image: .agent.scriptPods.image
+    }
+  }
+} | del(..|nulls)"
+
+NAMESPACE=octopus-agent-theagent
+RELEASE=theagent
+CHART="oci://docker.packages.octopushq.com/kubernetes-agent"
+
+MIGRATED_VALUES=`helm get values --namespace=$NAMESPACE $RELEASE -o json | jq $FILTER | jq .`
+echo $MIGRATED_VALUES
+
+
+helm upgrade --atomic --reset-then-reuse-values --namespace=$NAMESPACE $RELEASE --set-json "agent=$MIGRATED_VALUES" --version=2.*.* $CHART --dry-run --debug
