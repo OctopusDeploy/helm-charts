123433a913073b52a5f78bc7c0f23a6f4174ac6b
Trent Mohay <trent.mohay@octopus.com>
2024-08-10 17:28:07 +1000

    pwsh is producing valid json

--- charts/kubernetes-agent/migrations/v1_to_v2/upgrade_v1_to_v2.ps1
+++ charts/kubernetes-agent/migrations/v1_to_v2/upgrade_v1_to_v2.ps1
@@ -9,22 +9,27 @@ $JSON_OBJECT = ConvertFrom-Json $CURRENT_MANUALLY_SET_VALUES
 
 $DEPLOYMENT_TARGET = [PSCustomObject]@{
   enabled=$true;
-  count=5;
   initial = [PSCustomObject]@{
-    defaultNamespace = $JSON_OBJECT.agent.defaultNamespace;
-    environments = $JSON_OBJECT.agent.targetEnvironments;
-    tags = $JSON_OBJECT.agent.targetRoles;
-    tenantTags = $JSON_OBJECT.agent.targetTenantTags;
-    tenantedDeploymentParticipation = $JSON_OBJECT.agent.targetTenantedDeploymentParticipation;
+    defaultNamespace = $JSON_OBJECT.agent.defaultNamespace
+    environments = $JSON_OBJECT.agent.targetEnvironments
+    tags = $JSON_OBJECT.agent.targetRoles
+    tenantTags = $JSON_OBJECT.agent.targetTenantTags
+    tenantedDeploymentParticipation = "Untenanted"
     tenants = $JSON_OBJECT.agent.targetTenants
   }
 }
-$DEPLOYMENT_TARGET.initial.PSOBject.Properties
-#$JSON_OBJECT.agent | Add-Member -MemberType NoteProperty -TypeName string -Name name -Value $JSON_OBJECT.agent.targetName
-#$JSON_OBJECT.agent | Add-Member -MemberType NoteProperty -TypeName psobject -Name deploymentTarget -Value $DEPLOYMENT_TARGET
+$JSON_OBJECT.agent | Add-Member -MemberType NoteProperty -TypeName string -Name name -Value $JSON_OBJECT.agent.targetName
+$JSON_OBJECT.agent | Add-Member -MemberType NoteProperty -TypeName psobject -Name deploymentTarget -Value $DEPLOYMENT_TARGET
+$JSON_OBJECT.agent.PSObject.Properties.Remove("targetName") 
+$JSON_OBJECT.agent.PSObject.Properties.Remove("targetEnvironments") 
+$JSON_OBJECT.agent.PSObject.Properties.Remove("targetRoles") 
+$JSON_OBJECT.agent.PSObject.Properties.Remove("targetTenantTags") 
+$JSON_OBJECT.agent.PSObject.Properties.Remove("targetTenantedDeploymentParticipation") 
+$JSON_OBJECT.agent.PSObject.Properties.Remove("targetTenants") 
 #$JSON_OBJECT.agent.deploymentTarger
 #$JSON_OBJECT.agent | Add-Member -MemberType NoteProperty -TypeName object -Name deploymentTarget
 #$JSON_OBJECT.agent.deploymentTarget = New-Object PSObject -Property @{
 #    enabled = true
 #}
 #$JSON_OBJECT.agent.Name
+$JSON_OBJECT | ConvertTo-Json -Depth 5
