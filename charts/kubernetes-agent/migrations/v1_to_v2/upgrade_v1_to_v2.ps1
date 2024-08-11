848c55b4962fd6810bd13211ae1f8868c38755d3
Trent Mohay <trent.mohay@octopus.com>
2024-08-12 08:37:43 +1000

    output looks good

--- charts/kubernetes-agent/migrations/v1_to_v2/upgrade_v1_to_v2.ps1
+++ charts/kubernetes-agent/migrations/v1_to_v2/upgrade_v1_to_v2.ps1
@@ -3,21 +3,32 @@ $RELEASE="theagent"
 $CHART="oci://docker.packages.octopushq.com/kubernetes-agent"
 
 [string]$CURRENT_MANUALLY_SET_VALUES = helm get values --namespace $NAMESPACE $RELEASE -o json
-Write-host  $CURRENT_MANUALLY_SET_VALUES
+#Write-host  $CURRENT_MANUALLY_SET_VALUES
 
 $JSON_OBJECT = ConvertFrom-Json $CURRENT_MANUALLY_SET_VALUES
 
-$DEPLOYMENT_TARGET = [PSCustomObject]@{
-  enabled=$true;
-  initial = [PSCustomObject]@{
+$INITIAL_VALUES = [PSCustomObject]@{
     defaultNamespace = $JSON_OBJECT.agent.defaultNamespace
     environments = $JSON_OBJECT.agent.targetEnvironments
     tags = $JSON_OBJECT.agent.targetRoles
     tenantTags = $JSON_OBJECT.agent.targetTenantTags
     tenantedDeploymentParticipation = "Untenanted"
     tenants = $JSON_OBJECT.agent.targetTenants
-  }
 }
+
+$INITIAL_VALUES | ForEach-Object {
+    # Get array of names of object properties that can be cast to boolean TRUE
+    # PSObject.Properties - https://msdn.microsoft.com/en-us/library/system.management.automation.psobject.properties.aspx
+    $NonEmptyProperties = $_.psobject.Properties | Where-Object {$_.Value} | Select-Object -ExpandProperty Name
+    $REDUCED_INITIAL_VALUES = $_ | Select-Object -Property $NonEmptyProperties
+}
+
+$DEPLOYMENT_TARGET = [PSCustomObject]@{
+  enabled=$true;
+  initial = $REDUCED_INITIAL_VALUES
+}
+
+
 $JSON_OBJECT.agent | Add-Member -MemberType NoteProperty -TypeName string -Name name -Value $JSON_OBJECT.agent.targetName
 $JSON_OBJECT.agent | Add-Member -MemberType NoteProperty -TypeName psobject -Name deploymentTarget -Value $DEPLOYMENT_TARGET
 $JSON_OBJECT.agent.PSObject.Properties.Remove("targetName") 
@@ -26,10 +37,5 @@ $JSON_OBJECT.agent.PSObject.Properties.Remove("targetRoles")
 $JSON_OBJECT.agent.PSObject.Properties.Remove("targetTenantTags") 
 $JSON_OBJECT.agent.PSObject.Properties.Remove("targetTenantedDeploymentParticipation") 
 $JSON_OBJECT.agent.PSObject.Properties.Remove("targetTenants") 
-#$JSON_OBJECT.agent.deploymentTarger
-#$JSON_OBJECT.agent | Add-Member -MemberType NoteProperty -TypeName object -Name deploymentTarget
-#$JSON_OBJECT.agent.deploymentTarget = New-Object PSObject -Property @{
-#    enabled = true
-#}
-#$JSON_OBJECT.agent.Name
+
 $JSON_OBJECT | ConvertTo-Json -Depth 5
